dd7a0802df7d960a3ba1823ee178ea74cd26d446
Roman Yarullin <firefox883@gmail.com>
2017-02-07 15:15:48 +0000

    odin: Add support for TWRP
    Change-Id: I79eff0d0891fb437b7637dbfe35e0d59e4db960a

---
+++ board/twrp.mk
@@ -0,0 +1,5 @@
+# TWRP configurations
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TW_BRIGHTNESS_PATH := /sys/class/leds/lm3533-lcd-bl-1/brightness
+TW_SECONDARY_BRIGHTNESS_PATH := /sys/class/leds/lm3533-lcd-bl-2/brightness
+TW_THEME := portrait_hdpi
